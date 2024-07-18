e6022a9132583de7b321927f0caaddc83162985d
JBXamin <146653426+JBXamin@users.noreply.github.com>
2024-07-18 23:12:33 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-apt-get update
-apt-get install -y portaudio19-dev
-pip install -r requirements.txt
+echo "Startup script started" >> /home/site/wwwroot/startup.log
+apt-get update >> /home/site/wwwroot/startup.log 2>&1
+apt-get install -y portaudio19-dev >> /home/site/wwwroot/startup.log 2>&1
+pip install -r requirements.txt >> /home/site/wwwroot/startup.log 2>&1
+echo "Startup script completed" >> /home/site/wwwroot/startup.log
