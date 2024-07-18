f3e8e36d88252ddb11d40e8b7f73f3c239fbc73b
JBXamin <146653426+JBXamin@users.noreply.github.com>
2024-07-18 22:10:47 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Update package list and install PortAudio
 apt-get update
 apt-get install -y portaudio19-dev
-# Install Python dependencies
 pip install -r requirements.txt
-# Start the application (modify according to your app's entry point)
-gunicorn --bind 0.0.0.0:$PORT main:app
