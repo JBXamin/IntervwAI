074d492558f809899c0d7245ee2c82bbc659792f
JBXamin <146653426+JBXamin@users.noreply.github.com>
2024-07-22 17:06:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def sI():
 @app.route('/result/<session_id>')
 def result(session_id):
     responses = interview_results.get(session_id, [])
-    return render_template('iresult.html', responses=responses)
+    return render_template('Iresult.html', responses=responses)
 
 if __name__ == '__main__':
     app.run(debug=True)
