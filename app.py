e8a394afc7e4a7d1f16c466c345277d459986aa2
JBXamin <146653426+JBXamin@users.noreply.github.com>
2024-07-22 18:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,9 +142,6 @@ def askAns():
 def sI():
     return render_template('sI.html')
 
-def about():
-    return render_template('about.html')
-
 @app.route('/result/<session_id>')
 def result(session_id):
     responses = interview_results.get(session_id, [])
